abfd0c6cb1d9f2e3c3497171e173f3ce8280db8a
Diaa Mohammed <diaam852@gmail.com>
2025-11-06 16:25:18 +0200

    Do: article list and details pages

--- src/app/features/articles/article-details/article-details.component.ts
+++ src/app/features/articles/article-details/article-details.component.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ArticleService } from '../../../core/services/article.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-article-details',
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './article-details.component.html',
   styleUrl: './article-details.component.css'
 })
 export class ArticleDetailsComponent {
 
+  article: any;
+  isLoading = false;  
+  direction: 'rtl' | 'ltr' = 'rtl'; // default Arabic
+
+  constructor(private route: ActivatedRoute, private articleService: ArticleService) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.loadProject(+id);
+    }
+  }
+
+  loadProject(id: number) {
+    this.isLoading = true;
+    this.articleService.getById(id).subscribe({
+      next: (res) => {
+        this.article = res;
+        this.direction = this.detectLanguageDirection(res.content);
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.isLoading = false;
+      }
+    });
+  }
+  // Detect if content is mostly English or Arabic
+  detectLanguageDirection(text: string): 'rtl' | 'ltr' {
+    const arabicRegex = /[\u0600-\u06FF]/; // Arabic Unicode range
+    return arabicRegex.test(text) ? 'rtl' : 'ltr';
+  }
 }
--- src/app/features/articles/articles-list/articles-list.component.ts
+++ src/app/features/articles/articles-list/articles-list.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProjectService } from '../../../core/services/project.service';
+import { Router, RouterModule } from '@angular/router';
+import { ArticleService } from '../../../core/services/article.service';
 
 @Component({
-  selector: 'app-articles-list',
-  imports: [],
+  selector: 'app-projects-list',
+  standalone: true,
+  imports: [CommonModule,RouterModule],
   templateUrl: './articles-list.component.html',
   styleUrl: './articles-list.component.css'
 })
-export class ArticlesListComponent {
+export class ArticlesListComponent implements OnInit {
 
+  articles: any[] = [];
+  isLoading = false;
+
+  constructor(private articleService: ArticleService,private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadProjects();
+  }
+
+  loadProjects() {
+    this.isLoading = true;
+    this.articleService.getAll().subscribe({
+      next: (res) => {
+        this.articles = res.data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.isLoading = false;
+      }
+    });
+  }
+  openDetails(id: number) {
+    this.router.navigate(['/articles', id]);
+  }
 }
