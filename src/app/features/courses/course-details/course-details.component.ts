22b20cd0be653503f25a52e5e384e208b91bc1be
Diaa Mohammed <diaam852@gmail.com>
2025-11-07 16:26:56 +0200

    Do: courses list page and details

--- src/app/features/courses/course-details/course-details.component.ts
+++ src/app/features/courses/course-details/course-details.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CourseService } from '../../../core/services/course.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-course-details',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './course-details.component.html',
   styleUrl: './course-details.component.css'
 })
 export class CourseDetailsComponent {
+  course: any;
+  isLoading = false;  
+  direction: 'rtl' | 'ltr' = 'rtl'; // default Arabic
 
+  constructor(private route: ActivatedRoute, private courseService: CourseService) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.loadProject(+id);
+    }
+  }
+
+  loadProject(id: number) {
+    this.isLoading = true;
+    this.courseService.getById(id).subscribe({
+      next: (res) => {
+        this.course = res;
+        this.direction = this.detectLanguageDirection(res.description);
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.isLoading = false;
+      }
+    });
+  }
+  // Detect if content is mostly English or Arabic
+  detectLanguageDirection(text: string): 'rtl' | 'ltr' {
+    const arabicRegex = /[\u0600-\u06FF]/; // Arabic Unicode range
+    return arabicRegex.test(text) ? 'rtl' : 'ltr';
+  }
 }
--- src/app/features/courses/courses-list/courses-list.component.ts
+++ src/app/features/courses/courses-list/courses-list.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../course.model';
+import { CourseService } from '../../../core/services/course.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-courses-list',
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './courses-list.component.html',
   styleUrl: './courses-list.component.css'
 })
-export class CoursesListComponent {
+export class CoursesListComponent implements OnInit {
+  courses: Course[] = [];
+  loading = false;
 
+  constructor(private courseService: CourseService) {}
+
+  ngOnInit(): void {
+    this.loadCourses();
+  }
+
+  loadCourses(): void {
+    this.loading = true;
+    this.courseService.getAll().subscribe({
+      next: (res) => {
+        this.courses = res.data;
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      }
+    });
+  }
 }
